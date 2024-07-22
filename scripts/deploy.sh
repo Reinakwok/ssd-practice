1e189b5f7b529b31bfa0ba31efd4d32dbe694221
Reinakwok <reinakwok@gmail.com>
2024-07-23 02:34:06 +0800

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ set -x  # Print commands and their arguments as they are executed
 
 # Build and run a Docker container for the Flask application
 docker build -t my-flask-app .
-docker run -d -p 5000:5000 --name my-flask-app my-flask-app
+docker run -d -p 5001:5000 --name my-flask-app my-flask-app
 
 sleep 1  # Wait for 1 second
 
@@ -12,5 +12,5 @@ set +x  # Stop printing commands
 
 # Print instructions
 echo 'Now...'
-echo 'Visit http://localhost:5000 to see your Flask application in action.'
+echo 'Visit http://localhost:5001 to see your Flask application in action.'
 
