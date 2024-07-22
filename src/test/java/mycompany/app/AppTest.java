4263ebedb4938cdb1bddace19a4e9069b74b2ef9
Reinakwok <reinakwok@gmail.com>
2024-07-22 23:25:40 +0800

    add pom file

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -1,31 +1,52 @@
-import org.junit.After;
+package com.mycompany.app;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.After;
+import static org.junit.Assert.*;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AppTest {
 
-    private WebDriver driver;
+    WebDriver driver; 
+	WebDriverWait wait; 
+	String url = "http://172.24.0.1";
 
     @Before
     public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");  // Run in headless mode
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        driver = new ChromeDriver(options);
-        driver.get("http://172.24.0.1/index.php");
+        // ChromeOptions options = new ChromeOptions();
+        // options.addArguments("--headless");  // Run in headless mode
+        // options.addArguments("--no-sandbox");
+        // options.addArguments("--disable-dev-shm-usage");
+        // driver = new ChromeDriver(options);
+        driver = new HtmlUnitDriver(); 
+		wait = new WebDriverWait(driver, 10); 
+        //driver.get("http://172.24.0.1/index.php");
     }
+
+    @After
+    public void tearDown() { 
+		driver.quit(); 
+	}	 
+	
 
     @Test
     public void testHomePage() {
+        //get web page
+		driver.get(url);
+
+        wait.until(ExpectedConditions.titleContains("Login |"))
+
         WebElement passwordField = driver.findElement(By.id("password"));
         WebElement loginButton = driver.findElement(By.tagName("button"));
         
@@ -35,6 +56,8 @@ public class AppTest {
 
     @Test
     public void testInvalidPassword() {
+        driver.get(url);
+
         WebElement passwordField = driver.findElement(By.id("password"));
         WebElement loginButton = driver.findElement(By.tagName("button"));
 
@@ -47,13 +70,14 @@ public class AppTest {
 
     @Test
     public void testValidPassword() {
+
         WebElement passwordField = driver.findElement(By.id("password"));
         WebElement loginButton = driver.findElement(By.tagName("button"));
 
         passwordField.sendKeys("Complex@123");
         loginButton.click();
 
-        assertEquals("http://172.24.0.1/dashboard.php", driver.getCurrentUrl());
+        assertEquals(url+"/dashboard.php", driver.getCurrentUrl());
 
         WebElement welcomeMessage = driver.findElement(By.tagName("p"));
         assertTrue(welcomeMessage.getText().contains("Your password: Complex@123"));
